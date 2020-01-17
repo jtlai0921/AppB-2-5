d9f1f13c7b3592add2ce48612f4e59fdf4970092
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-17 16:51:40 +0800

    Initial commit

---
+++ 我的主控台程式/Class1.vb
@@ -0,0 +1,16 @@
+﻿Public Class MyName
+    Private FirstName, LastName As String
+    Public Sub New(ByVal f As String, ByVal l As String)
+        FirstName = f : LastName = l
+    End Sub
+    Public Function GetName() As String
+        Return "姓名： " & LastName & FirstName
+    End Function
+End Class
+
+Partial Class MyName
+    Public Function GetFirstName() As String
+        Return "名： " & FirstName
+    End Function
+End Class
+
---
+++ 我的主控台程式/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' 宣告MyName類別的物件
+        Dim name As New MyName("會安", "陳")
+        ' 呼叫物件方法
+        txtOutput.Text = name.GetName() & vbNewLine
+        txtOutput.Text &= name.GetFirstName() & vbNewLine
+        txtOutput.SelectionStart = 0
+    End Sub
+End Class
